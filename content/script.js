548cfeb349c5a8a79705b62896854c2ea8642cf3
Gabz047 <gabe1612mail@gmail.com>
2023-06-20 23:59:00 -0300

    att2.6

--- content/script.js
+++ content/script.js
@@ -9,3 +9,17 @@ function menuShow() {
     }
 }
 
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.description-I', {duration: 1000});
+sr.reveal('.description2-I', {duration: 1000});
+sr.reveal('.imgref1', {duration: 1000});
+sr.reveal('.img-container-I img', {duration: 1000});
+sr.reveal('.text-1-athom', {duration: 1000});
+sr.reveal('.text-2-athom', {duration: 1000});
+sr.reveal('.img-athom', {duration: 1000});
+sr.reveal('.video-duvida', {duration: 1000});
+sr.reveal('.box-duvidas', {duration: 1000});
+sr.reveal('.content-box-qmsomos', {duration: 1000});
+sr.reveal('.box-qmsomos', {duration: 1000});
+sr.reveal('.box-rdsociais', {duration: 1000});
